15459c6c802e7808480946230c3f2d76fb03ec36
avijitchoudhury <avijit.choudhury24@gmail.com>
2019-10-09 23:35:47 +0000

    refactored App.js and added functionality to post tweets without needing to refresh

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,40 +1,65 @@
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function () {
 
- $(document).ready(function () {
-  
-//   const data = [
-//     {
-//       "user": {
-//         "name": "Newton",
-//         "avatars": "https://i.imgur.com/73hZDYK.png"
-//         ,
-//         "handle": "@SirIsaac"
-//       },
-//       "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//       },
-//       "created_at": 1461116232227
-//     },
-//     {
-//       "user": {
-//         "name": "Descartes",
-//         "avatars": "https://i.imgur.com/nlhLi3I.png",
-//         "handle": "@rd"
-//       },
-//       "content": {
-//         "text": "Je pense , donc je suis"
-//       },
-//       "created_at": 1461113959088
-//     }
-//   ]
+  //   const data = [
+  //     {
+  //       "user": {
+  //         "name": "Newton",
+  //         "avatars": "https://i.imgur.com/73hZDYK.png"
+  //         ,
+  //         "handle": "@SirIsaac"
+  //       },
+  //       "content": {
+  //         "text": "If I have seen further it is by standing on the shoulders of giants"
+  //       },
+  //       "created_at": 1461116232227
+  //     },
+  //     {
+  //       "user": {
+  //         "name": "Descartes",
+  //         "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //         "handle": "@rd"
+  //       },
+  //       "content": {
+  //         "text": "Je pense , donc je suis"
+  //       },
+  //       "created_at": 1461113959088
+  //     }
+  //   ]
 
-  
+  let renderTweets = function (arr) {
+    for (let tweet of arr) {
+      createTweetElement(tweet)
+    }
+  }
+
+  let loadTweets = async function () {
+    await $.ajax({
+      url: `/tweets/`,
+      method: 'GET',
+      data: 'data',
+      dataType: 'JSON'
+    }).then((data) => {
+      renderTweets(data);
+    })
+  }
+
+  let postNewTweet = async function (data) {
+    await $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      data: data
+    })
+    await loadTweets();
+  }
+
   const createTweetElement = function (tweet) {
-     let markUp = `<article class="articleTweet">
+    let markUp = `<article class="articleTweet">
           <header class="mainHeader">
     
             <div class="img_container">
@@ -62,52 +87,27 @@
           </footer>
     
         </article>`
-        $('#tweetContainer').append(markUp)
+    $('#tweetContainer').prepend(markUp)
+  }
+
+  let tweetForm = $(".tweetForm")
+  tweetForm.submit(function (event) {
+    event.preventDefault();
+    let textChar = tweetForm.serialize();
+    let slicedText = textChar.slice(5, textChar.length)
+    if (slicedText.length === 0) {
+      return alert("enter some text")
+    } else if (slicedText.length > 140) {
+      return alert("Character count exceed. ERROR!")
     }
 
-    let renderTweets = function (arr) {
-      for(let tweet of arr) {
-        createTweetElement(tweet)
-      }
-    }
-    
-    let tweetForm = $(".tweetForm")
-    tweetForm.submit( async function(event) {
-      event.preventDefault();
-      let textChar = tweetForm.serialize();
-      let slicedText = textChar.slice(5, textChar.length)
-      if(slicedText.length === 0){
-        return alert("enter some text")
-      } else if (slicedText.length > 140) {
-        return alert("Character count exceed. ERROR!")
-      }
-      try {
-        const response = await $.ajax ({
-          url: `/tweets/`,
-          type: 'POST',
-          dataType: 'JSON',
-          data: tweetForm.serialize()
-        })
-      } catch (error) {
-        console.error(error)
-      }   
-    })
-    
-      let loadTweets = async function () {
-      
-         await $.ajax ({
-          url: `/tweets/`,
-          type: 'GET',
-          data: 'data',
-          dataType: 'JSON'
-        }) .then((data) => {
-          renderTweets(data);
-        })  
-    }  
-    loadTweets(); 
+    postNewTweet(textChar);
+  })
 
-    
+  loadTweets();
 });
+
+
 
 
 
