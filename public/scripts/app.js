99f30f0ac5c809e9ab45c311572b7748512c7e47
avijitchoudhury <avijit.choudhury24@gmail.com>
2019-10-09 00:38:34 +0000

    made dynamic tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,21 +4,80 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Test / driver code (temporary). Eventually will get this from the server.
-// const tweetData = {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//     "handle": "@SirIsaac"
-//   },
-//   "content": {
-//     "text": "If I have seen further it is by standing on the shoulders of giants"
-//   },
-//   "created_at": 1461116232227
-// }
+$(document).ready(function () {
+  
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
-// const $tweet = createTweetElement(tweetData);
+  let renderTweets = function (arr) {
+    for(let tweet of arr) {
+      createTweetElement(tweet)
+    }
+  }
 
-// // Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  const createTweetElement = function (tweet) {
+   let markUp = `<article class="articleTweet">
+        <header class="mainHeader">
+  
+          <div class="img_container">
+            <img class="raphael" src=${tweet.user.avatars}>
+            <span class="tweet_display_name">${tweet.user.name}</span>
+            <span class="handler">${tweet.user.handle}</span>
+          </div>
+
+        </header>
+
+        <div class="tweet_content">
+          <p>
+            ${tweet.content.text}
+          </p>
+        </div>
+        <footer>
+          <div class="tweet_footer">
+            <span>10 days ago</span>
+            <div class="img_footer">
+              <img src="/images/flag.png" alt="">
+              <img src="/images/retweet-arrows-symbol.png" alt="">
+              <img src="/images/heart.png" alt="">
+            </div>
+          </div>
+        </footer>
+
+      </article>`
+      $('#tweetContainer').append(markUp)
+  }
+
+  renderTweets(data);
+})
+
+
+
+
+
+
+
+
+
